a6fac38b1dda81c478fc6b74ab42c58044eec64a
binja <benjaminnamdar@gmail.com>
2026-03-30 11:02:04 +0300

    Scaffold monorepo with React client, NestJS server and shared code
    Sets up the three packages:
    
    - client: Create React App TypeScript template
    - server: Nest CLI starter app
    - shared: CardType and HeroClass enums
    
    Also adds a root .gitignore and SVG diagrams of the data model and
    interfaces under documentation/.

---
+++ shared/src/enums.ts
@@ -0,0 +1,18 @@
+enum CardType {
+    Hero = "Hero",
+    Item = "Item",
+    Magic = "Magic",
+    Modifier = "Modifier",
+    Challenge = "Challenge",
+    Leader = "Leader",
+    Monster = "Monster",
+}
+
+enum HeroClass {
+    Fighter = "Fighter",
+    Guardian = "Guardian",
+    Ranger = "Ranger",
+    Thief = "Thief",
+    Wizard = "Wizard",
+    Bard = "Bard"
+}
